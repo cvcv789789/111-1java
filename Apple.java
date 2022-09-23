e12c14510bbbda588f43110cb97362df6c9ca5ed
cvcv789789 <85999964+cvcv789789@users.noreply.github.com>
2022-09-23 11:57:35 +0800

    Add files via upload

--- Apple.java
+++ Apple.java
@@ -3,6 +3,10 @@ public class Apple {
     public static void main(String[] args) {
         System.out.println(args[0]);
         System.out.println(args[1]);	
-		  System.out.println(args[3]);	
+		printstr("cccc");
+    }
+	public static void printstr(String str) {
+        System.out.println("my name" + str);
+       
     }
 } 
