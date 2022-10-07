a40eae250398bfaf0deb1c72882ebb1ad07fc661
cvcv789789 <85999964+cvcv789789@users.noreply.github.com>
2022-10-07 11:41:42 +0800

    加分

---
+++ Test4.java
@@ -0,0 +1,14 @@
+ 
+import java.util.Scanner;
+public class Test4 {
+    public static void main(String[] args) {
+		int grade=0;
+		String a = new String();
+		while(grade<10){	
+			System.out.println(a+grade);
+			a=a + " ";
+			grade=grade+1;
+		}
+    }
+ 
+} 
