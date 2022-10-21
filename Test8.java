512319cfd443b885070a078a61f99e6931697fd5
cvcv789789 <85999964+cvcv789789@users.noreply.github.com>
2022-10-21 09:25:06 +0800

    1021

--- Test8.java
+++ Test8.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
 public class Test8 {
     public static void main(String[] args) {
-		int row=0,b,c;
+		int row=0,b,c=-1,d;
 		String a = "*";
-		while(row<9){		 
+		while(row<11){		 
 			 
 			if(row >5){
 				b=row-5;
+				c= c - 2;
 			}else{
-				b= 4- row;
+				b= 5- row;
+				c= c + 2;
 			}
-			
+			d= c; 
 			while(b>0){		 
 				System.out.print(" ");
 				b=b-1;
 			}
-			
-			System.out.println(a);
-			a= a + "**";
+			while(d>0){		 
+				System.out.print("*");
+				d=d-1;
+				 
+			}
+			 
+			System.out.println("");
+			 
 			row=row+1;
 		}
     }
  
 } 
+/*
+  *
+ ***
+*****
+ ***
+  * 
+  */
