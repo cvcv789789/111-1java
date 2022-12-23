516a05008407b8dcaef861fe3c1f2f2b0d23501d
cvcv789789 <85999964+cvcv789789@users.noreply.github.com>
2022-12-23 11:36:38 +0800

    1223

---
+++ Test19.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+ 
+public class Test19 {
+    public static void main(String[] args) {
+		int[]myArr = {7,5,4,9,8,1,2};
+		int num=1;
+		int input;
+		System.out.print("幾個為一組:");
+		Scanner s = new Scanner(System.in);
+		input = s.nextInt();
+		int num1 = myArr.length/input;
+		 
+		float avg=0,total=0;
+		for(int i=0;i<myArr.length;i++){
+		 
+			if (num1 != 0){
+				if (num <=input){
+					total=myArr[i]+total;
+					num=num+1;
+				}else{
+					num=1;
+					avg= total/input;
+					total=0;
+					System.out.print(" "+avg+" ,");
+					num1=num1-1;
+				} 
+			}else{
+				System.out.print(" "+myArr[i]+" ,");
+			}
+			
+			 
+			
+			
+		}	
+		 
+    }
+ 
+}
